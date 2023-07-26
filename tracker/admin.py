d2daaac960cf3e44bc308702fad4d6120f95ae45
neostyle88 <makssimm1@gmail.com>
2023-07-26 18:19:02 +0200

    register all models in tracker admin.py

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from tracker.models import Airport, Crew, Country, City, Route, AirplaneType, \
+    Airplane, Order, Flight, Ticket
+
+admin.site.register(Crew)
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Airport)
+admin.site.register(Route)
+admin.site.register(AirplaneType)
+admin.site.register(Airplane)
+admin.site.register(Order)
+admin.site.register(Flight)
+admin.site.register(Ticket)
