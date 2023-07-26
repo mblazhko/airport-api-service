c898f165c088ab4de0c4c4760981edffe08c55e9
neostyle88 <makssimm1@gmail.com>
2023-07-26 20:52:42 +0200

    add serializers to tracker serializers.py: - AirportFacilitySerializer - AirplaneFacility - AirportSerializer - AirportListSerializer - AirportDetailSerializer

--- tracker/serializers.py
+++ tracker/serializers.py
@@ -22,3 +22,45 @@ class CitySerializer(serializers.ModelSerializer):
         fields = ("id", "name", "country")
 
 
+class AirportFacilitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airport
+        fields = ("id", "name")
+
+
+class AirplaneFacility(serializers.ModelSerializer):
+    class Meta:
+        model = Airplane
+        fields = ("id", "name")
+
+
+class AirportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airport
+        fields = ("id", "name", "facilities", "closest_big_city")
+
+
+class AirportListSerializer(AirportSerializer):
+    country = serializers.CharField(
+        source="closest_big_city.country.name",
+        read_only=True
+    )
+    facilities = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="name"
+    )
+
+    class Meta:
+        model = Airport
+        fields = ("id", "name", "country", "closest_big_city" "facilities")
+
+
+class AirportDetailSerializer(AirportSerializer):
+    country = serializers.CharField(
+        source="closest_big_city.country.name",
+        read_only=True
+    )
+    facilities = AirportFacilitySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Airport
+        fields = ("id", "name", "country", "closest_big_city", "facilities")
