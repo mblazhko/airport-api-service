9cf3b4c1da8a0f188fd99b51d04f96eab19ea5c4
neostyle88 <makssimm1@gmail.com>
2023-07-26 17:36:40 +0200

    add Country, City, Airport models

--- tracker/models.py
+++ tracker/models.py
@@ -8,3 +8,24 @@ class Crew(models.Model):
         return f"{self.first_name} {self.last_name}"
 
 
+class Country(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=255)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}({self.country.name})"
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255)
+    closest_big_city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}({self.closest_big_city.name})"
