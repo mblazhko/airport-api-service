37c9e7e35ef8ef830d2161598614df2620549060
neostyle88 <makssimm1@gmail.com>
2023-07-26 20:06:25 +0200

    set unique to true in name of AirplaneType

--- tracker/models.py
+++ tracker/models.py
@@ -46,7 +46,7 @@ class Route(models.Model):
 
 
 class AirplaneType(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
