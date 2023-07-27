712f42016b01932714fa81a52273cb6af20787cc
neostyle88 <makssimm1@gmail.com>
2023-07-27 18:45:41 +0200

    register passenger model in tracker admin.py

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from tracker.models import Airport, Crew, Country, City, Route, AirplaneType, \
-    Airplane, Order, Flight, Ticket
+    Airplane, Order, Flight, Ticket, Passenger
 
 admin.site.register(Crew)
 admin.site.register(Country)
@@ -13,3 +13,4 @@ admin.site.register(Airplane)
 admin.site.register(Order)
 admin.site.register(Flight)
 admin.site.register(Ticket)
+admin.site.register(Passenger)
