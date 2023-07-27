8042de6cdd89d51274b147e3fbeb0af7b83c6938
neostyle88 <makssimm1@gmail.com>
2023-07-27 21:03:45 +0200

    replace two Facility urls for Airport and Airplane by one in trackers urls.py

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,8 +5,6 @@ from tracker.views import (
     CrewViewSet,
     CountryViewSet,
     CityViewSet,
-    AirplaneFacilityViewSet,
-    AirportFacilityViewSet,
     AirportViewSet,
     RouteViewSet,
     AirplaneViewSet,
@@ -15,14 +13,14 @@ from tracker.views import (
     FlightViewSet,
     PassengerViewSet,
     TicketViewSet,
+    FacilityViewSet,
 )
 
 router = routers.DefaultRouter()
 router.register("crews", CrewViewSet)
 router.register("countries", CountryViewSet)
 router.register("cities", CityViewSet)
-router.register("airplane_facilities", AirplaneFacilityViewSet)
-router.register("airport_facilities", AirportFacilityViewSet)
+router.register("facilities", FacilityViewSet)
 router.register("airports", AirportViewSet)
 router.register("routes", RouteViewSet)
 router.register("airplanes", AirplaneViewSet)
@@ -32,9 +30,7 @@ router.register("flights", FlightViewSet)
 router.register("passengers", PassengerViewSet)
 router.register("tickets", TicketViewSet)
 
-urlpatterns = [
-    path("", include(router.urls))
-]
+urlpatterns = [path("", include(router.urls))]
 
 
 app_name = "tracker"
