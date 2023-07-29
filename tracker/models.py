f173cff6bc8cb9d060db877b4013970da2f647a0
neostyle88 <makssimm1@gmail.com>
2023-07-29 19:59:55 +0200

    set unique to true to name field of Facility model

--- tracker/models.py
+++ tracker/models.py
@@ -42,7 +42,7 @@ class City(models.Model):
 
 
 class Facility(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
