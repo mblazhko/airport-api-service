f41deee22f47335099128298006b731af0b91dc2
neostyle88 <makssimm1@gmail.com>
2023-07-30 11:54:08 +0200

    change creation method of country in sample_city method

--- tracker/tests/model_samples.py
+++ tracker/tests/model_samples.py
@@ -26,7 +26,11 @@ def sample_country(**params):
 
 
 def sample_city(**params):
-    defaults = {"name": "Kyiv", "country": sample_country()}
+    country, created = Country.objects.get_or_create(name="Ukraine")
+    defaults = {
+        "name": "Kyiv",
+        "country": country,
+    }
     defaults.update(params)
     return City.objects.create(**defaults)
 
@@ -51,12 +55,8 @@ def sample_route(**params):
     defaults = {
         "source": sample_airport(),
         "destination": sample_airport(
-            {
-                "name": "LWO",
-                "city": sample_city(
-                    {"name": "Lviv", "country": sample_country()}
-                ),
-            }
+            name="LWO",
+            city=sample_city(name="Lviv", country=sample_country()),
         ),
         "distance": 700,
     }
@@ -77,7 +77,7 @@ def sample_airplane(**params):
         "seat_in_row": 6,
         "seat_letters": ["A", "B", "C", "D", "E", "F"],
         "facilities": sample_facility(),
-        "airplane_type": sample_airplane_type
+        "airplane_type": sample_airplane_type,
     }
     defaults.update(params)
     return Airplane.objects.create(**defaults)
@@ -91,7 +91,7 @@ def sample_flight(**params):
         "arrival_time": "2020-01-03T00:00:00",
         "crews": [sample_crew()],
         "terminal": "D",
-        "gate": 10
+        "gate": 10,
     }
     defaults.update(params)
     return Flight.objects.create(**defaults)
