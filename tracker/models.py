87a8d65754cc4ac8ea01dab5f25a08dd3f79f6a8
neostyle88 <makssimm1@gmail.com>
2023-07-30 19:52:26 +0200

    remove unique=True from name field of AirplaneType model

--- tracker/models.py
+++ tracker/models.py
@@ -85,7 +85,7 @@ class Route(models.Model):
 
 
 class AirplaneType(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
