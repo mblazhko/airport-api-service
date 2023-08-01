6165889d76afe545abd5b1e8214952fb041c1078
neostyle88 <makssimm1@gmail.com>
2023-08-01 12:10:21 +0200

    add test_user_api.py to test User api

---
+++ user/tests/test_user_api.py
@@ -0,0 +1,66 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+from django.urls import reverse
+
+class UserAPITest(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        # Create a regular user
+        self.user = get_user_model().objects.create_user(
+            email="regular@example.com",
+            password="testpassword"
+        )
+        # Create a superuser
+        self.superuser = get_user_model().objects.create_superuser(
+            email="superuser@example.com",
+            password="testpassword"
+        )
+
+    def test_create_regular_user(self):
+        url = reverse("user:create")
+        data = {
+            "email": "newuser@example.com",
+            "password": "testpassword",
+        }
+
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(get_user_model().objects.count(), 3)  # Two users already created in setUp.
+
+    def test_create_superuser(self):
+        url = reverse("user:create")
+        data = {
+            "email": "superuser2@example.com",
+            "password": "testpassword",
+            "is_staff": True,
+            "is_superuser": True,
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(get_user_model().objects.count(), 3)  # Two users already created in setUp.
+
+    def test_retrieve_user_details_authenticated(self):
+        url = reverse("user:manage")
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["email"], self.user.email)
+
+    def test_retrieve_user_details_unauthenticated(self):
+        url = reverse("user:manage")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_login_with_valid_credentials(self):
+        url = reverse("user:token_obtain_pair")
+        data = {
+            "email": "regular@example.com",
+            "password": "testpassword",
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("access", response.data)
+        self.assertIn("refresh", response.data)
